0bb635402659773ebd84130b9739c0f2796200cf
Alexander Pyzhkin <120998812+Alexander-Pyzhkin@users.noreply.github.com>
2022-12-19 07:47:35 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,9 @@
+﻿let d = new Date();
+console.log(d);
+
+
+
+if (d >= 1672518300000) {
+    document.getElementById("cont").style.display = "block";
+    document.getElementById("ps").style.display = "block";
+};
